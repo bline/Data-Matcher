a3ec62f7e501e1c4daa30f6a030b0ee466143ee0
Scott Beck <scottbeck@gmail.com>
2010-06-13 16:36:17 -0400

    Setup for Pod::Weaver

--- lib/Moose/Meta/Attribute/Custom/Trait/CanConfigure.pm
+++ lib/Moose/Meta/Attribute/Custom/Trait/CanConfigure.pm
@@ -1,3 +1,5 @@
+use strict;
+#ABSTRACT: internal trait
 package Moose::Meta::Attribute::Custom::Trait::CanConfigure;
 sub register_implementation {'Data::Matcher::Meta::Attribute::Trait::CanConfigure'}
 
@@ -5,35 +7,12 @@ sub register_implementation {'Data::Matcher::Meta::Attribute::Trait::CanConfigur
 
 __END__
 
-=head1 NAME
-
-Moose::Meta::Attribute::Custom::Trait::CanConfigure - tells moose about our custom
-trait
-
-=head1 VERSION
-
-version 0.01
-
 =head1 DESCRIPTION
 
 This module tells Moose about L<Data::Matcher::Meta::Attribute::Trait::CanConfigure>
 
 Nothing to look at here.. move along.
 
-
-=head1 AUTHOR
-
-Scott Beck E<lt>scottbeck@gmail.comE<gt>
-
-
-=head1 COPYRIGHT
-
-Copyright 2011 by Scott A Beck E<lt>scottbeck@gmail.comE<gt>.
-
-This program is free software; you can redistribute it and/or
-modify it under the same terms as Perl itself.
-
-See F<http://www.perl.com/perl/misc/Artistic.html>
 
 =begin Pod::Coverage
 
