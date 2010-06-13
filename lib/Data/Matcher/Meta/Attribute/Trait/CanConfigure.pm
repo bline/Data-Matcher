04d8c583a7d689b303f51e8a792b2909000d3c0b
Scott Beck <scottbeck@gmail.com>
2010-06-13 16:31:51 -0400

    setup to use Pod::Weaver

--- lib/Data/Matcher/Meta/Attribute/Trait/CanConfigure.pm
+++ lib/Data/Matcher/Meta/Attribute/Trait/CanConfigure.pm
@@ -1,3 +1,6 @@
+use strict;
+#ABSTRACT: trait for attributes which can be set via option parsing.
+
 package Data::Matcher::Meta::Attribute::Trait::CanConfigure;
     use Moose::Role;
     use MooseX::Has::Sugar;
@@ -9,11 +12,6 @@ package Data::Matcher::Meta::Attribute::Trait::CanConfigure;
 1;
 __END__
 
-=head1 NAME
-
-Data::Matcher::Meta::Attribute::Trait::CanConfigure - trait for attributes which
-can be set via option parsing.
-
 =head1 DESCRIPTION
 
 This module sets up traits for configurable attributes that can be either
@@ -21,21 +19,6 @@ local or global. Global attributes are ones that are setup during the initial
 parsing of matching rules. Local attributes are attributes that
 can be set while matching data. This can be very useful to change how the data
 is matched. See L<Data::Matcher/Rules> for more details.
-
-=head1 AUTHOR
-
-Scott Beck E<lt>scottbeck@gmail.comE<gt>
-
-
-=head1 COPYRIGHT
-
-Copyright 2011 by Scott A Beck E<lt>scottbeck@gmail.comE<gt>.
-
-This program is free software; you can redistribute it and/or
-modify it under the same terms as Perl itself.
-
-See F<http://www.perl.com/perl/misc/Artistic.html>
-
 
 =cut
 
